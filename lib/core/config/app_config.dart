eb76ae91be9e21868f5bc32a5b9403391810b2b7
josephus <josephusmupanda48@gmail.com>
2025-05-20 12:01:37 +0200

    add api key config

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppConfig {
   static const String appName = 'Fleet Monitoring';
   static const String apiBaseUrl = 'https://mockapi.io/projects';
   static const String carsEndpoint = '/cars';
-  static const String googleMapsApiKey = 'AIzaSyA4_Y5sh-dDKwXUuxSPRFCFRb4afYfEgEg';
+  // static  String mapsKey = dotenv.env['GOOGLE_MAPS_API_KEY']!;
   static const String fontFamily = 'Poppins';
 }
